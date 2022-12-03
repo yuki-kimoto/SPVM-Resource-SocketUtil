9704008d9bef01856f030fe0ec5cb3dbeacbea60
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-12-04 07:34:53 +0900

    Initial commit

---
+++ t/lib/SPVM/TestCase/Resource/SocketUtil.c
@@ -0,0 +1,14 @@
+#include "spvm_native.h"
+
+
+
+int32_t SPVM__TestCase__Resource__SocketUtil__test(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  stack[0].ival = 1;
+  
+  return 0;
+}
+
+
