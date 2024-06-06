143010a1988db0a6b320f1beda72f289559ef895
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-06-06 09:48:33 +0900

    Add documents for spvm_socket_util.h.
      [Document Improvmenet]
        * Add documents for spvm_socket_util.h.

--- lib/SPVM/Resource/SocketUtil.native/include/spvm_socket_util.h
+++ lib/SPVM/Resource/SocketUtil.native/include/spvm_socket_util.h
@@ -35,10 +35,38 @@
 
 int32_t spvm_socket_errno (void);
 
-void* spvm_socket_strerror_string (SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length);
+void* spvm_socket_strerror_string(SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length);
+
+/* spvm_socket_strerror_string
+
+  Gets an error string as an SPVM string given the error number <error_number> and the max length of the error string <length> and returns the error string.
+  
+  If <length> is 0, It is adjusted automatically.
+  
+  In Linux and Unix, this function calls strerror_string native API.
+  
+  In Windows, this function calls FormatMessageA function.
+  
+*/
 
 const char* spvm_socket_strerror(SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number, int32_t length);
 
+/* spvm_socket_strerror
+
+  Calls spvm_socket_strerror_string function and gets data of char[] from its return value and returns the data.
+  
+  If the return value of spvm_socket_strerror_string function is NULL, returns NULL.
+
+*/
+
 const char* spvm_socket_strerror_nolen(SPVM_ENV* env, SPVM_VALUE* stack, int32_t error_number);
+
+/* spvm_socket_strerror_nolen
+
+  Alias for the following code.
+  
+  const char* ret = spvm_socket_strerror(env, stack, error_number, 0);
+  
+*/
 
 #endif
